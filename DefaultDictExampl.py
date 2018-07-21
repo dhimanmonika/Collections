fddde71867b7a0f03098a159f26a514896f30ffb
Dhiman <dhimam@amazon.com>
2018-07-21 15:31:16 +0530

    msg

--- DefaultDictExampl.py
+++ DefaultDictExampl.py
@@ -1,4 +1,6 @@
-"""The standard dictionary includes the method setdefault() for retrieving a value and establishing a default if the value does not exist. By contrast, defaultdict lets the caller specify the default up front when the container is initialized."""
+"""The standard dictionary includes the method setdefault() for retrieving a value and establishing a default
+ if the value does not exist. By contrast, defaultdict lets the caller specify the default
+ up front when the container is initialized."""
 
 import collections
 
--- NamedTupleExmpl.py
+++ NamedTupleExmpl.py
@@ -1,4 +1,5 @@
-"""Named tuples assign meaning to each position in a tuple and allow for more readable, self-documenting code. They can be used wherever regular tuples are used, and they add the ability to access fields by name instead of position index."""
+"""Named tuples assign meaning to each position in a tuple and allow for more readable, self-documenting code.
+ They can be used wherever regular tuples are used, and they add the ability to access fields by name instead of position index."""
 
 from collections import namedtuple
 import csv
